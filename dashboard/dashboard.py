0be0bdab7dd3f8f7e427ecf8cefb4c9045d5536a
danendrafau <danendrafaharutama@gmail.com>
2024-12-26 16:58:04 +0700

    Upload dashboard main data

---
+++ dashboard/dashboard.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from streamlit_option_menu import option_menu
+from babel.numbers import format_currency
+sns.set(style='dark')
